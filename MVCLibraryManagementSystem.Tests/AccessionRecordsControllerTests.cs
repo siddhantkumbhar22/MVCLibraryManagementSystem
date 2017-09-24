8253aa94674a2ae904fd7d945f3efd93adc941cc
Dhaval Anjaria <dhv2712@gmail.com>
2017-09-24 21:56:23 +0530

    Created tests for Acc. Records and Books Controllers
    These tests check some basic functionality that the controllers' action
    methods must provide. A lot of these tests are commented out. This is
    because for migrations and what not the project needs to compile. The
    proper solution to this is to use repositories and view models and mocks,
    which was my original solution, however considering that the project would
    balloon too quickly, it has not been implemented yet. More tests to come

---
+++ MVCLibraryManagementSystem.Tests/AccessionRecordsControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCLibraryManagementSystem.Models;
+using MVCLibraryManagementSystem.DAL;
+using MVCLibraryManagementSystem.Controllers;
+
+namespace MVCLibraryManagementSystem.Tests
+{
+    [TestClass]
+    public class AccessionRecordsControllerTests
+    {
+
+        // This is why Repos are needed, so that we don't access this directly
+        private LibraryContext context = new LibraryContext();
+
+        /// <summary>
+        /// By default the Book Index view doesn't list items. This makes sure it does
+        /// </summary>
+        [TestMethod]
+        public void TestBookIndexHasItems()
+        {
+            AccessionRecordsController controller = new AccessionRecordsController();
+
+            var viewResult = controller.Index() as ViewResult;
+
+            IEnumerable<AccessionRecord> recordsReturned = (IEnumerable<AccessionRecord>)viewResult.Model;
+
+            foreach (var AccessionRecord in recordsReturned)
+            {
+                Assert.IsNotNull(AccessionRecord.Item.Title);
+            }
+        }
+
+        /// <summary>
+        /// This code will not compile, however it needs to because Index neads a search searchString
+        /// </summary>
+        [TestMethod]
+        public void TestAccessionRecordIndexHasSearch()
+        {
+            AccessionRecordsController controller = new AccessionRecordsController();
+
+            // var viewResult = controller.Index(searchString: "Test") as ViewResult;
+            // IEnumerable<AccessionRecord> recordsReturned = (IEnumerable<AccessionRecord>)viewResult.Model;
+            // Collections.Assert.IsSubsetOf(recordsReturned, context.AccessionRecords.ToList());
+        }
+
+        /// <summary>
+        /// This code will not compile, however it needs to because Index neads a search searchString
+        /// </summary>
+        [TestMethod]
+        public void TestAccessionRecordIndexHasPagination()
+        {
+            AccessionRecordsController controller = new AccessionRecordsController();
+
+            // var viewResult = controller.Index(3) as ViewResult;
+            // IEnumerable<AccessionRecord> recordsReturned = (IEnumerable<AccessionRecord>)viewResult.Model;
+
+            // Assert.AreEqual(10, recordsReturned.Count);
+
+        }
+    }
+}
---
+++ MVCLibraryManagementSystem.Tests/BooksControllerTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCLibraryManagementSystem.Models;
+using MVCLibraryManagementSystem.DAL;
+using MVCLibraryManagementSystem.Controllers;
+
+namespace MVCLibraryManagementSystem.Tests
+{
+    [TestClass]
+    public class BooksControllerTests
+    {
+        // This is why Repos are needed, so that we don't access this directly
+        private LibraryContext context = new LibraryContext();
+
+        /// <summary>
+        /// By default the Book Index view doesn't list items. This makes sure it does
+        /// </summary>
+        [TestMethod]
+        public void TestBookIndexHasItems()
+        {
+            BooksController controller = new BooksController();
+
+            var viewResult = controller.Index() as ViewResult;
+
+            IEnumerable<Book> booksReturned = (IEnumerable<Book>)viewResult.Model;
+
+            foreach(var book in booksReturned)
+            {
+                Assert.IsNotNull(book.Item.Title);
+            }
+        }
+
+        /// <summary>
+        /// This code will not compile, however it needs to because Index neads a search searchString
+        /// </summary>
+        [TestMethod]
+        public void TestBookIndexHasSearch()
+        {
+            BooksController controller = new BooksController();
+
+            // var viewResult = controller.Index(searchString: "Test") as ViewResult;
+            // IEnumerable<Book> booksReturned = (IEnumerable<Book>)viewResult.Model;
+            // Collections.Assert.IsSubsetOf(booksReturned, context.Books.ToList());
+        }
+
+        /// <summary>
+        /// This code will not compile, however it needs to because Index neads a search searchString
+        /// </summary>
+        [TestMethod]
+        public void TestBookIndexHasPagination()
+        {
+            BooksController controller = new BooksController();
+
+            // var viewResult = controller.Index(3) as ViewResult;
+            // IEnumerable<Book> booksReturned = (IEnumerable<Book>)viewResult.Model;
+
+            // Assert.AreEqual(10, booksReturned.Count);
+
+        }
+    }
+}
