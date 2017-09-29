bc022f7d422031ab72271e5c45d3bda3380719eb
Dhaval Anjaria <dhv2712@gmail.com>
2017-09-29 22:08:54 +0530

    Created IssuedItem tests
    This commit adds tests for the Issue Item functionality. It adds a Model,
    the Service, IService and the default, generated Controller. However, it
    has been left intentionally incomplete. This is because how an item (or
    rather it's copy) should be selected to be issued remains debatable. I
    don't know the best way for it and I believe it should be discussed with
    the team.

--- MVCLibraryManagementSystem/Models/IssuedItem.cs
+++ MVCLibraryManagementSystem/Models/IssuedItem.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCLibraryManagementSystem.Models
 {
     public class IssuedItem
     {
         public int IssuedItemId { get; set; }
-        public virtual Item Item { get; set; }
+        [Required]
+        public virtual AccessionRecord AccessionRecord { get; set; }
+        [Required]
         public virtual Member Member { get; set; }
         public DateTime IssueDate { get; set; }
+        public DateTime DateReturned { get; set; }
         public int LateFeePerDay { get; set; }
+        public bool IsIssued { get; set; }
     }
 }
